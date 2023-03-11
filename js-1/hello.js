1e4f79e185beb9ae29e5cb7e41544ab6062535b8
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-03-10 19:23:54 -0500

    Solucion a los ejercicios de JS

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+// escribe la función hola acá
+function hola(name) {
+  console.log("Hola " + name + "!");
+}
+// código de prueba
+console.log(hola("Pedro")); // "Hola Pedro!"
+console.log(hola("Juan")); // "Hola Juan!"
+console.log(hola("")); // "Hola !"
---
+++ js-1/sum.js
@@ -0,0 +1,9 @@
+// escribe la función suma acá
+function suma(num) {
+  let result = (num * (num + 1)) / 2;
+  return result;
+}
+// código de prueba
+console.log(suma(4)); // 1 + 2 + 3 + 4 = 10
+console.log(suma(10)); // 55
+console.log(suma(15)); // 120
